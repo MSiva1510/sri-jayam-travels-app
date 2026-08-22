0156fff1f6f8e9cbdf8cb822b842699e7fe8ef2d
siva <sivadon9791270@gmail.com>
2026-08-22 13:22:06 +0530

    extra

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '../../core/config/supabase_config.dart';
-import '../../providers/auth_provider.dart';
 import '../driver/home_screen.dart';
 
 class LoginScreen extends ConsumerStatefulWidget {
